0f53a66b0b7b28bb6293d3529a7fcf934741cb2a
mahi jindal <127783778+18mahi@users.noreply.github.com>
2026-05-20 17:20:24 +0530

    Update file.py

--- python/Challenges/Challenge-03/Contact-Manager/file.py
+++ python/Challenges/Challenge-03/Contact-Manager/file.py
@@ -38,79 +38,79 @@ def Menu_System():
     print("6. Favourite Contacts")
     print("7. Exit")
     
-input_choice=input("Enter your choice: ")
-while input_choice != "6":
-    #add contact option
-    if input_choice == "1":
-        duplicate=False
-        name = input("Enter name: ")
-        try:
-            if name=="":
-                raise ValueError("Name cannot be empty.")
-        except ValueError as e:
-                print(e)
-                continue
-        phone = input("Enter phone: ")
-        email = input("Enter email: ")
-        city = input("Enter city: ")
-        # Check for duplicate contact
-        for contact in contacts:
-            if contact["name"] == name:
-                print("Contact with this name already exists.")
-                duplicate = True
-                break
-        if not duplicate:
-            add_contact(name, phone, email,city)
-        
-    #view contacts list
-    elif input_choice == "2":
-        sorted_contacts = sorted(contacts, key=lambda x: x["name"].lower())
-        view_contact()
-        print(f'Total contacts: {len(contacts)}')
-        
-    #search contact 
-    elif input_choice == "3":
-        search_name = input("Enter name to search: ")
-        contact = search_contact(search_name)
-        if contact:
-            print(contact)
-        else:
-            print("Contact not found.")
-                
-    #update contact
-    elif input_choice == "4":
-        update_name = input("Enter name to update: ")
-        contact = search_contact(update_name)
-        if contact:
-            new_phone = input("Enter new phone: ")
-            new_email = input("Enter new email: ")
-            new_city = input("Enter new city: ")
-            contact["phone"] = new_phone
-            contact["email"] = new_email
-            contact["city"] = new_city
-        else:
-            print("Contact not found.")
+    input_choice=input("Enter your choice: ")
+    while input_choice != "6":
+        #add contact option
+        if input_choice == "1":
+            duplicate=False
+            name = input("Enter name: ")
+            try:
+                if name=="":
+                    raise ValueError("Name cannot be empty.")
+            except ValueError as e:
+                    print(e)
+                    continue
+            phone = input("Enter phone: ")
+            email = input("Enter email: ")
+            city = input("Enter city: ")
+            # Check for duplicate contact
+            for contact in contacts:
+                if contact["name"] == name:
+                    print("Contact with this name already exists.")
+                    duplicate = True
+                    break
+            if not duplicate:
+                add_contact(name, phone, email,city)
             
-    #delete contact
-    elif input_choice == "5":
-        delete_name = input("Enter name to delete: ")
-        contact = search_contact(delete_name)
-        if contact:
-            contacts.remove(contact)
-            print("Contact deleted successfully.")
+        #view contacts list
+        elif input_choice == "2":
+            sorted_contacts = sorted(contacts, key=lambda x: x["name"].lower())
+            view_contact()
+            print(f'Total contacts: {len(contacts)}')
+            
+        #search contact 
+        elif input_choice == "3":
+            search_name = input("Enter name to search: ")
+            contact = search_contact(search_name)
+            if contact:
+                print(contact)
+            else:
+                print("Contact not found.")
+                    
+        #update contact
+        elif input_choice == "4":
+            update_name = input("Enter name to update: ")
+            contact = search_contact(update_name)
+            if contact:
+                new_phone = input("Enter new phone: ")
+                new_email = input("Enter new email: ")
+                new_city = input("Enter new city: ")
+                contact["phone"] = new_phone
+                contact["email"] = new_email
+                contact["city"] = new_city
+            else:
+                print("Contact not found.")
+                
+        #delete contact
+        elif input_choice == "5":
+            delete_name = input("Enter name to delete: ")
+            contact = search_contact(delete_name)
+            if contact:
+                contacts.remove(contact)
+                print("Contact deleted successfully.")
+            else:
+                print("Contact not found.")
+        elif input_choice == "6":
+            favourite_contacts=[]
+            fav_name = input("Enter name to add to favourite contacts: ")
+            for contact in contacts:
+                if contact["name"] == fav_name:
+                    favourite_contacts.append(contact)
+                    print("Contact added to favourites.")
+                    break
+            else:
+                print("Contact not found.")
         else:
-            print("Contact not found.")
-    elif input_choice == "6":
-        favourite_contacts=[]
-        fav_name = input("Enter name to add to favourite contacts: ")
-        for contact in contacts:
-            if contact["name"] == fav_name:
-                favourite_contacts.append(contact)
-                print("Contact added to favourites.")
-                break
-        else:
-            print("Contact not found.")
-    else:
-        print("Thank you for using Contact Manager!")
+            print("Thank you for using Contact Manager!")
     
-    Menu_System()
+Menu_System()
