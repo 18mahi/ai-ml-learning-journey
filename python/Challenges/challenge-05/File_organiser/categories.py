f2e2f4d7e73c43fc9f9f688f104fbed0537a45cf
mahi jindal <127783778+18mahi@users.noreply.github.com>
2026-06-09 15:35:54 +0530

    Create categories.py

---
+++ python/Challenges/challenge-05/File_organiser/categories.py
@@ -0,0 +1,10 @@
+file_types = {
+    "Images": [".jpg", ".png", ".jpeg"],
+    "Videos": [".mp4", ".mkv", ".avi"],
+    "Documents": [".pdf", ".docx", ".txt"],
+    "Archives": [".zip", ".rar", ".7z"],
+    "Code": [".py", ".html", ".css", ".js"],
+    "Audio": [".mp3", ".wav"],
+    "Software": [".exe", ".apk"],
+    "Others": []  # This category will hold files that don't fit into the above categories
+}
